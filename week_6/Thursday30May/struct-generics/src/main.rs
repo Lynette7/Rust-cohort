1fce7357e655b1f8f913f4af3ae17544084dd74d
Lynette Mwangi <69351261+Lynette7@users.noreply.github.com>
2024-07-17 16:16:12 +0300

    swapping in struct generics

--- week_6/Thursday30May/struct-generics/src/main.rs
+++ week_6/Thursday30May/struct-generics/src/main.rs
@@ -10,11 +10,11 @@ impl<T> Point<T> {
         Point{x, y}
     }
 
-    fn get_x(&self) ->&T {
+    fn get_x(&self) -> &T {
         &self.x
     }
 
-    fn get_y(&self) ->&T {
+    fn get_y(&self) -> &T {
         &self.y
     }
 }
@@ -30,6 +30,19 @@ fn main() {
     let direction = Compass::new(8.8, "south west");
     println!("N is at position {}", direction.get_n());
     println!("S is at position {}", direction.get_s());
+
+    // If you want to swap the values in the different attributes
+    let mut a = 1;
+    let mut b = 2;
+    swap(&mut a, &mut b);
+    println!("a = {} and b = {}", a, b);
+
+    // if we want to swap values of similar attributes of different objects
+    let mut car1 = Vehicle::new("KCY", "KDD".to_string());
+    let car2 = Vehicle::new("KAZ", "GK01".to_string());
+    car1.swapcar(car2);
+    assert_eq!(car1.customer_one, "KAZ");
+    println!("Customer One car1 {:?}", car1);
 }
 
 // How to implement struct generics so that you can have different types for different attributes
@@ -50,4 +63,26 @@ impl<T, D> Compass<T, D> {
     fn get_s(&self) ->&D {
         &self.s
     }
+}
+
+// If you want to swap the values in the different attributes
+fn swap<T> (x: &mut T, y: &mut T) {
+    std::mem::swap(x, y);
+}
+
+// if we want to swap values of similar attributes of different objects
+#[derive(Debug)]
+struct Vehicle<T, X>{
+    customer_one: T,
+    customer_two: X
+}
+
+impl<T,X> Vehicle<T, X>{
+    fn new(customer_one: T, customer_two: X) -> Self {
+        Vehicle{customer_one, customer_two}
+    }
+
+    fn swapcar(& mut self, other_client: Vehicle<T, X>) {
+        self.customer_one = other_client.customer_one;
+    } 
 }
